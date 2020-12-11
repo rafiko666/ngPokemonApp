8e0ce931fb942f30ec7b0e7bd357fffcba39c995
Tchoketch Rafik <fr_tchoketch_kebir@esi.dz>
2020-12-11 10:15:12 +0100

    add routing Back()

--- src/app/pokemon/detail-pokemon/detail-pokemon.component.ts
+++ src/app/pokemon/detail-pokemon/detail-pokemon.component.ts
@@ -3,7 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Pokemon } from '../../pokemon.model';
 import { POKEMONS } from '../../shared/list.pokemons';
 import { PokemonsService } from '../pokemons.service';
-
+import {Location} from '@angular/common'
 @Component({
   selector: 'app-detail-pokemon',
   templateUrl: './detail-pokemon.component.html',
@@ -12,11 +12,15 @@ import { PokemonsService } from '../pokemons.service';
 export class DetailPokemonComponent implements OnInit {
   listOfPokemons : Pokemon [] = null;
   pokemonToDisplay : Pokemon = null;
-  constructor(private route:ActivatedRoute,private router:Router,private pokemonsService:PokemonsService) { }
+  constructor(private route:ActivatedRoute,private location:Location,private pokemonsService:PokemonsService) { }
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
     this.pokemonToDisplay = this.pokemonsService.getSinglePokemon(id);
+  }
+
+  goBack(){
+    this.location.back()
   }
 
 }
